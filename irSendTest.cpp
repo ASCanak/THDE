5f363ecbc0cf46bca776acb0d818f93619be1f6a
AymaneMachrouki <59871096+AymaneMachrouki@users.noreply.github.com>
2020-10-26 15:38:55 +0100

    Update irSendTest.cpp

--- irSendTest.cpp
+++ irSendTest.cpp
@@ -1,5 +1,4 @@
 #include "hwlib.hpp"
-#include <iostream>
 #include <bitset>
 using namespace std;
 
@@ -11,27 +10,20 @@ int getBit(uint32_t b,  int n){
         return 0;
 }
 
-double getOnTime(unsigned int i){
-    if(int i == 1)
+double getDelays(unsigned int i){
+    if(i == 1)
         return 1.6;
-    else if(int i == 0)
+    else
         return 0.8;
-}
-
-double getOffTime(unsigned int i){
-    if(int i == 1)
-        return 0.8
-    else if(int i == 0)
-        return 1.6
 }
 
 int main( void ){
     // IR output LED
-    auto ir = hwlib::target::d2_36kHz();
+    auto ir = hwlib::target::d2_36kHz(); //verandert naar 38kHz
     // red output LED
-    auto red = hwlib::target::pin_out( hwlib::target::pins::d42 );
-    // switch which enables the 36 kHz output
-    auto sw = hwlib::target::pin_in( hwlib::target::pins::d43 );
+    auto red = hwlib::target::pin_out( hwlib::target::pins::d51 );
+    // switch which enables the 38 kHz output
+    auto sw = hwlib::target::pin_in( hwlib::target::pins::d45 );
     
     unsigned int irBericht = 0b10011111100111111001111110011111;
     
@@ -40,25 +32,30 @@ int main( void ){
         
         while(! sw.read()){
             ir.write(1);
+            ir.flush();
             red.write(1);
+            red.flush();
             
             hwlib::wait_ms(9);
             
             ir.write(0);
+            ir.flush();
             red.write(0);
             
             hwlib::wait_ms(4.5);
             
             for(int i = 31; i >= 0 ; i--){
                 ir.write(1);
+                ir.flush();
                 red.write(1);
 
-                hwlib::wait_ms(getOnTime(getBit(irBericht, i)));
+                hwlib::wait_ms(getDelays(getBit(irBericht, i)));
 
                 red.write(0);
                 ir.write(0);
+                ir.flush();
                 
-                hwlib::wait_ms(getOffTime(getBit(irBericht, i)));
+                hwlib::wait_ms(getDelays(getBit(irBericht, i) + 1));
             }
             hwlib::wait_ms(4);
         }
