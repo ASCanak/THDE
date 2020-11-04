acf102b171e1f6ec8ebf5cc9f3613ff9b5195a40
ivo <60585949+ivo2002@users.noreply.github.com>
2020-11-04 13:56:15 +0100

    Update OLED.hpp

--- THDE_Lasertag_TEMP/Display_test/OLED.hpp
+++ THDE_Lasertag_TEMP/Display_test/OLED.hpp
@@ -2,38 +2,44 @@
 #include "rtos.hpp"
 namespace target = hwlib::target;
 
+struct hitInfo {unsigned int plrID; unsigned int data; unsigned int hp;};
+
 class oledTaak : public rtos::task<>{
 private:
 rtos::flag oledFlag;
-rtos::pool<char> oledPool;
+rtos::pool<hitInfo> hitPool;
+rtos::pool<unsigned int> timePool;
 
 public:
-    oledTaak(): task("oledTaak"), 
-    oledFlag(this, "oledFlag"), 
-    oledPool("oledPool")
+    oledTaak(): 
+        task(5, "oledTaak"),
+        oledFlag(this, "oledFlag"), 
+        oledPool("oledPool")
     {}
 
     void write_Stats(unsigned int plrID, unsigned int data, unsigned int hp){
-        oledPool.write(plrID, data, hp); oledFlag.set();
+        hitInfo x{plrID, data, hp};
+        hitPool.write(x); oledFlag.set();
         hwlib::cout << "Hit by plrID: " << plrID << "With wpnPwr: " << data << "HP left: << hp << \n";
-        scherm1 << "\f" << oledPool.read() << "\n";
+        scherm1 << "\f" << hitPool.read() << "\n";
     }
-         void write_Time(unsigned int time){
-         oledPool.write(text); oledFlag.set();
+    void write_Time(unsigned int time){
+         timePool.write(time); oledFlag.set();
          hwlib::cout << "Remaining time: " << "\n";
-         scherm2 << "\f" << oledPool.read() << "\n";
+         scherm2 << "\f" << timePool.read() << "\n";
     }
+    void 
 
 
 private:
     void main(){ // auto kan niet 
-        auto scl = target::pin_oc( target::pins::scl );
-        auto sda = target::pin_oc( target::pins::sda );
+        target::pin_oc scl = target::pin_oc( target::pins::scl );
+        target::pin_oc sda = target::pin_oc( target::pins::sda );
 
-        auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda( scl,sda );
+        hwlib::i2c_bus_bit_banged_scl_sda i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda( scl,sda );
 
-        auto oled = hwlib::glcd_oled( i2c_bus, 0x3c );
-        auto f2 = hwlib::font_default_8x8();
-        auto scherm1 = hwlib::terminal_from(oled, f2);
+        hwlib::glcd_oled oled = hwlib::glcd_oled( i2c_bus, 0x3c );
+        hwlib::font_default_8x8 f2 = hwlib::font_default_8x8();
+        hwlib::terminal_from scherm1 = hwlib::terminal_from(oled, f2);
     }
 };
