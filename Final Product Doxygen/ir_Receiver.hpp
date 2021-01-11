a59e207efc78be03af6d81371b3b0b14ed340469
ivo <60585949+ivo2002@users.noreply.github.com>
2021-01-11 17:12:00 +0100

    Added Doxygen

--- Final Product Doxygen/ir_Receiver.hpp
+++ Final Product Doxygen/ir_Receiver.hpp
@@ -1,3 +1,9 @@
+/// This file contains Doxygen lines
+/// \brief
+/// ir-Reciever class
+/// \details
+/// This class recieves signals from the ir-led 
+
 #ifndef IR_RECEIVER_HPP
 #define IR_RECEIVER_HPP
 
@@ -14,6 +20,10 @@ private:
 public:
     ir_Receiver(hwlib::target::pin_in &tsop_signal, hwlib::target::pin_out &tsop_gnd, hwlib::target::pin_out &tsop_vdd);
     
+    /// \brief
+    /// Gets current sgnal
+    /// \details
+    /// This function reads the current signal from the ir-led
     bool getCurrentSignal();
 };
 
