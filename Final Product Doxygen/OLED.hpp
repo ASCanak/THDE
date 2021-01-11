bf88c9db278ad7561e7057aa3f046264a5c0a700
AymaneMachrouki <59871096+AymaneMachrouki@users.noreply.github.com>
2021-01-11 17:47:28 +0100

    Added doxygen

--- Final Product Doxygen/OLED.hpp
+++ Final Product Doxygen/OLED.hpp
@@ -41,37 +41,37 @@ public:
     /// \brief
     /// Writes the hit info to a pool
     /// \details
-    /// This function makes a struct of the three parameters and writes the struct to a pool and sets a flag.
+    /// This function makes a struct of the three parameters and writes the struct to a pool and sets a flag
     void write_HitInfo(unsigned int plrID, unsigned int data, unsigned int hp);
     
     /// \brief
     /// Writes the total minutes to a pool
     /// \details
-    /// This function writes the total minutes to a pool and sets a flag.
+    /// This function writes the total minutes to a pool and sets a flag
     void write_Min(unsigned int updated_Time);
     
     /// \brief
     /// Writes the total seconds to a pool
     /// \details
-    /// This function writes the total seconds to a pool and sets a flag.
+    /// This function writes the total seconds to a pool and sets a flag
     void write_Sec(unsigned int updated_Time);
     
     /// \brief
     /// Writes the plrID to a pool
     /// \details
-    /// This function write the user inputted plrID to a pool and sets a flag.
+    /// This function write the user inputted plrID to a pool and sets a flag
     void write_plrID(unsigned int plrID);
     
     /// \brief
     /// Writes the wpnPwrID to a pool
     /// \details
-    /// This function write the user inputted wpnPwrID to a pool and sets a flag.
+    /// This function write the user inputted wpnPwrID to a pool and sets a flag
     void write_wpnPwrID(unsigned int data);
     
     /// \brief
     /// Shows all necessary infomation on the OLED 
     /// \details
-    /// This function writes all the necessary infomation on to the OLED. 
+    /// This function writes all the necessary infomation on to the OLED
     void write_to_Oled(unsigned int EnemyID, unsigned int EnemywpnPwr, unsigned int myHP, unsigned int min, unsigned int sec, unsigned int myplrID, unsigned int mywpnPwr);
 };
 #endif
