e504f4ce824a08f4cabef1b3926c9a7e5f72aa5e
ivo <60585949+ivo2002@users.noreply.github.com>
2021-01-11 17:30:01 +0100

    Added Doxygen

--- Final Product Doxygen/parameterControl.hpp
+++ Final Product Doxygen/parameterControl.hpp
@@ -1,3 +1,9 @@
+/// This file contains Doxygen lines
+/// \brief
+/// ParameterControl class 
+/// \details
+/// This class sends the recieved data to the entity class and gives gameControl the start signal
+
 #ifndef PARAMETERCONTROL_HPP
 #define PARAMETERCONTROL_HPP
 
@@ -15,11 +21,24 @@ private:
 
 public:
     parameterControl(gameControl &gameCtrl, gameInfoEntity &gameEntity);
-
-    void sendMessage(unsigned int data);
-
-    void set_plrID(unsigned int plrID);
     
+    /// \brief
+    /// Sends data to a different class
+    /// \details
+    /// This function sends either the startgame data to the gameControl class 
+    /// or sends the gameTime data to the entity class.
+    void sendMessage(unsigned int data);
+    
+    /// \brief
+    /// Sets plrID
+    /// \details
+    /// This function sends the plrID to the entity    
+    void set_plrID(unsigned int plrID);
+     
+    /// \brief
+    /// Sets wpnPwrID
+    /// \details
+    /// This function sends the wpnPwrID to the entity 
     void set_wpnPwr(unsigned int data);
 };
 
