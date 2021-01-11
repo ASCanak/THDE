6ff707e7f569e601c72b65f7891ee269db47fd21
AymaneMachrouki <59871096+AymaneMachrouki@users.noreply.github.com>
2021-01-11 17:13:31 +0100

    Added doxygen

--- Final Product Doxygen/ir_Led.hpp
+++ Final Product Doxygen/ir_Led.hpp
@@ -1,3 +1,9 @@
+/// This file contains Doxygen lines
+/// \brief
+/// Ir_led class
+/// \details
+/// This is a Ir-led class that inherits from rtos::task
+/// This class turns the Led on or off
 #ifndef IR_LED_HPP
 #define IR_LED_HPP
 
@@ -13,8 +19,16 @@ private:
 public:
     ir_Led(hwlib::target::d2_36kHz &ir, hwlib::target::pin_out &led);
 
+    /// \brief
+    /// Turns led on
+    /// \details
+    /// This function turns the led on
     void zetAan();
 
+    /// \brief
+    /// Turns led off 
+    /// \details
+    /// This function turns the led off
     void zetUit();
 };
 #endif
